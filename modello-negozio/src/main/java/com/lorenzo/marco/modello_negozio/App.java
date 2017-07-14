56d9bd165a7b14feac5bf729fa49d2d424536e35
vigna <vigna@vigna-HP-Pavilion-dv6-Notebook-PC>
2017-07-14 10:12:38 +0200

    Aggiunto progetto con file pom e .travis

---
+++ modello-negozio/src/main/java/com/lorenzo/marco/modello_negozio/App.java
@@ -0,0 +1,13 @@
+package com.lorenzo.marco.modello_negozio;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
